c71f51116980a64d7007aeecadee88adc1e5a9e1
basel1322 <78343797+basel1322@users.noreply.github.com>
2021-02-16 13:53:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-var bigtosmall = true, numbersbigtosmall = true, oldmovie = null;
-var submitbutton = document.querySelector("#test").addEventListener('click', addtolist);
-var sortbutton = document.querySelector("#sort").addEventListener('click', sortnames);
+var sortfromSmalltoBig = true; oldmovie = null;
+var addmoviebutton = document.querySelector("#test").addEventListener('click', addRowToTable);
+var buttonForSortingByMovietitle = document.querySelector("#sort").addEventListener('click', sortTableByMovieTitle);
 var mainlist = document.querySelector("#main-list");
-var sortbuttonfornumbers = document.querySelector("#sort-by-numbers").addEventListener('click', sortnumbers);
+var buttonForSortingByRating = document.querySelector("#sort-by-numbers").addEventListener('click', sortTableByRating);
 var result = document.querySelector("#inputtext");
 
 
@@ -10,20 +10,20 @@ mainlist.addEventListener('click', (e) => {
     target = e.target;
     if (target.classList[0] === "delete-item" || target.classList[0] === "edit-item") {
         parent = target.parentElement;
-        parent = findparent(parent);
+        parent = findParent(parent);
         if (target.classList[0] === "delete-item")
             parent.remove();
         else if (target.classList[0] === "edit-item") {
-            restorevalues();
-            newbuild(parent);
+            restoreOldValues();
+            createNewElementsForEditing(parent);
         }
     }
     else if (target.classList[0] === "Save-item") {
-        savenewvalues();
+        saveChangesAfterEditting();
 
     }
     else if (target.classList[0] === "cancel-item") {
-        restorevalues();
+        restoreOldValues();
     }
 
 
@@ -32,103 +32,100 @@ mainlist.addEventListener('click', (e) => {
 
 
 
-function addtolist() {
+function addRowToTable() {
     const moviename = document.querySelector("#Item-for-list");
     const movierate = document.querySelector("#rate-for-movie");
-    if (inputcheck(moviename, movierate)) {
-        let maintr = document.createElement("tr");
-        maintr.classList.add("item");
-        let movieth = document.createElement("td");
-        movieth.classList.add("moviename");
-        movieth.textContent = moviename.value;
-        maintr.append(movieth);
-        let rateli = document.createElement("td");
-        rateli.classList.add("movierate");
-        rateli.textContent = movierate.value;
-        maintr.append(rateli);
-        thfordeletebutton = document.createElement("td");
+    if (inputCheck(moviename, movierate)) {
+        let parent = document.createElement("tr");
+        parent.classList.add("item");
+        let movieTitletd = document.createElement("td");
+        movieTitletd.classList.add("moviename");
+        movieTitletd.textContent = moviename.value;
+        parent.append(movieTitletd);
+        let movieRateTd = document.createElement("td");
+        movieRateTd.classList.add("movierate");
+        movieRateTd.textContent = movierate.value;
+        parent.append(movieRateTd);
+        tdfordeletebutton = document.createElement("td");
         deletebutton = document.createElement("button");
         editbutton = document.createElement("button");
         editbutton.classList.add("edit-item");
         deletebutton.classList.add("delete-item");
         deletebutton.textContent = "Delete";
         editbutton.textContent = "Edit";
-        thfordeletebutton.append(deletebutton);
-        thfordeletebutton.append(editbutton);
-        maintr.append(thfordeletebutton);
-        mainlist.append(maintr);
+        tdfordeletebutton.append(deletebutton);
+        tdfordeletebutton.append(editbutton);
+        parent.append(tdfordeletebutton);
+        mainlist.append(parent);
     }
 
 
 }
 
-function sortnames() {
-    restorevalues();
+function sortTableByMovieTitle() {
+    restoreOldValues();
     var list = document.getElementsByClassName("item");
-    flag = true;
-    while (flag) {
-        flag = false;
+    issorted = true;
+    while (issorted) {
+        issorted = false;
         for (let i = 0; i < list.length - 1; i++) {
-            if (bigtosmall == true) {
+            if (sortfromSmalltoBig == true) {
                 if (list[i].firstChild.textContent.toLowerCase() > list[i + 1].firstChild.textContent.toLowerCase()) {
-                    switchpos(list[i], list[i + 1]);
-                    flag = true;
+                    switchPos(list[i], list[i + 1]);
+                    issorted = true;
                 }
             }
             else
                 if (list[i].firstChild.textContent.toLowerCase() < list[i + 1].firstChild.textContent.toLowerCase()) {
-                    switchpos(list[i], list[i + 1]);
-                    flag = true;
+                    switchPos(list[i], list[i + 1]);
+                    issorted = true;
                 }
         }
     }
-    bigtosmall = !bigtosmall;
+    sortfromSmalltoBig = !sortfromSmalltoBig;
 }
 
-function switchpos(firstkid, secondkid) {
+function switchPos(firstkid, secondkid) {
     mainlist.insertBefore(secondkid, firstkid);
 }
-function sortnumbers() {
-    restorevalues();
+function sortTableByRating() {
+    restoreOldValues();
     var list = document.getElementsByClassName("item");
-    flag = true;
-    while (flag) {
-        flag = false;
+    issorted = true;
+    while (issorted) {
+        issorted = false;
         for (let i = 0; i < list.length - 1; i++) {
-            if (numbersbigtosmall == true) {
+            if (sortfromSmalltoBig == true) {
                 if (+(list[i].children[1].textContent) > +(list[i + 1].children[1].textContent)) {
-                    switchpos(list[i], list[i + 1]);
-                    flag = true;
+                    switchPos(list[i], list[i + 1]);
+                    issorted = true;
                 }
             }
             else
                 if (+(list[i].children[1].textContent) < +(list[i + 1].children[1].textContent)) {
-                    switchpos(list[i], list[i + 1]);
-                    flag = true;
+                    switchPos(list[i], list[i + 1]);
+                    issorted = true;
                 }
         }
     }
-    numbersbigtosmall = !numbersbigtosmall;
+    sortfromSmalltoBig = !sortfromSmalltoBig;
 
 }
-function inputcheck(moviename, movierate) {
-
-    let flag = false;
+function inputCheck(moviename, movierate) {
     if (movierate.value != "" && moviename.value != "") {
         if (isNaN(movierate.value) || movierate.value < 0 || movierate.value > 10) {
-
-            text = "Please Movie Rating must be 0-10";
-            result.textContent = text;
             result.style.color = "red";
             movierate.style.borderColor = "red";
+            result.textContent = "Please Movie Rating must be 0-10";
+            return false;
         } else {
-            flag = true;
+
             movierate.style.borderColor = "black";
             moviename.style.borderColor = "black";
             result.style.color = "black";
-            text = "Your Movie have been added ";
+            result.textContent = "Your Movie have been added ";
+            return true;
         }
-        result.textContent = text;
     }
     else if (movierate.value != "" || moviename.value != "") {
         if (movierate.value != "") {
@@ -144,54 +141,40 @@ function inputcheck(moviename, movierate) {
             result.style.color = "red";
 
         }
-
+        return false;
     }
     else {
-        text = "Please you have to put Movie name and rating from 0-10";
         movierate.style.borderColor = "red";
         moviename.style.borderColor = "red";
         result.style.color = "red";
-        result.textContent = text;
+        result.textContent = "Please you have to put Movie name and rating from 0-10";
     }
-    return flag;
 }
 
-function findparent() {
-    let flag = true;
-    while (flag) {
+function findParent() {
+    let isFound = true;
+    while (isFound) {
         if (parent.classList[0] === "item")
-            flag = false;
+            isFound = false;
         else
             parent = parent.parentElement;
 
     }
     return parent;
 }
-function newbuild(parent) {
+function createNewElementsForEditing(parent) {
     oldmovie = {};
     oldmovie.movietitle = parent.children[0].textContent;
     oldmovie.movierate = parent.children[1].textContent;
     oldmovie.parent = parent;
-    parent.children[1].textContent = parent.children[0].textContent = "";
-    parent.children[2].children[0].textContent = "Save";
-    parent.children[2].children[0].classList.remove("delete-item");
-    parent.children[2].children[0].classList.add("Save-item");
-    parent.children[2].children[1].textContent = "Cancel";
-    parent.children[2].children[1].classList.remove("edit-item");
-    parent.children[2].children[1].classList.add("cancel-item");
-    let newmovietitle = document.createElement("INPUT");
-    newmovietitle.classList.add("tableinput");
-    newmovietitle.value = oldmovie.movietitle;
-    parent.children[0].append(newmovietitle);
-    let newmovierate = document.createElement("INPUT");
-    newmovierate.classList.add("tableinput");
-    newmovierate.value = oldmovie.movierate;
-    parent.children[1].append(newmovierate);
+    addSaveCancelButtonsToRow(parent);
+    addTwoInputsTooRow(parent, oldmovie);
+
 
 }
-function restorevalues() {
+function restoreOldValues() {
     if (oldmovie != null) {
-        restorebuttons();
+        restoreEditDeleteButtonsToRow();
         oldmovie.parent.children[1].firstChild.remove();
         oldmovie.parent.children[1].textContent = oldmovie.movierate;
         oldmovie.parent.children[0].firstChild.remove();
@@ -201,7 +184,7 @@ function restorevalues() {
 
 
 }
-function restorebuttons() {
+function restoreEditDeleteButtonsToRow() {
     oldmovie.parent.children[2].children[0].textContent = "Delete";
     oldmovie.parent.children[2].children[0].classList.remove("Save-item");
     oldmovie.parent.children[2].children[0].classList.add("delete-item");
@@ -209,21 +192,41 @@ function restorebuttons() {
     oldmovie.parent.children[2].children[1].classList.remove("cancel-item");
     oldmovie.parent.children[2].children[1].classList.add("edit-item");
 }
-function savenewvalues() {
-    restorebuttons();
-
+function saveChangesAfterEditting() {
+    restoreEditDeleteButtonsToRow();
     let newtitle = oldmovie.parent.children[0].firstChild.value;
     let newrate = +(oldmovie.parent.children[1].firstChild.value);
-    if (newtitle != "" && newrate != "" && Number.isInteger(newrate) && newrate > 0 && newrate < 10) {
+    if (checkNewValues(newrate, newtitle)) {
         oldmovie.parent.children[1].firstChild.remove();
         oldmovie.parent.children[1].textContent = newrate;
         oldmovie.parent.children[0].firstChild.remove();
         oldmovie.parent.children[0].textContent = newtitle;
         oldmovie = null;
-
     }
-    else{
-        restorevalues();
-    result.textContent = "Your Request Failed . Please Check that Movie title is not empty and Movie rate from 0-10";
+    else {
+        restoreOldValues();
+        result.textContent = "Your Request Failed . Please Check that Movie title is not empty and Movie rate from 0-10";
     }
+}
+function checkNewValues(newrate, newtitle) {
+    return (newtitle != "" && newrate != "" && Number.isInteger(newrate) && newrate > 0 && newrate < 10)
+}
+function addSaveCancelButtonsToRow(parent) {
+    parent.children[1].textContent = parent.children[0].textContent = "";
+    parent.children[2].children[0].textContent = "Save";
+    parent.children[2].children[0].classList.remove("delete-item");
+    parent.children[2].children[0].classList.add("Save-item");
+    parent.children[2].children[1].textContent = "Cancel";
+    parent.children[2].children[1].classList.remove("edit-item");
+    parent.children[2].children[1].classList.add("cancel-item");
+}
+function addTwoInputsTooRow() {
+    let newmovietitle = document.createElement("INPUT");
+    newmovietitle.classList.add("tableinput");
+    newmovietitle.value = oldmovie.movietitle;
+    parent.children[0].append(newmovietitle);
+    let newmovierate = document.createElement("INPUT");
+    newmovierate.classList.add("tableinput");
+    newmovierate.value = oldmovie.movierate;
+    parent.children[1].append(newmovierate);
 }
